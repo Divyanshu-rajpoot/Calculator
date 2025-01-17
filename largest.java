d242dc51176677d1a7e0d683dc9d91472e3ecddb
Divyanshu-rajpoot <divy.r.007@gmail.com>
2025-01-17 17:40:29 +0530

    largest

---
+++ largest.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class largest {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+        if (num > num2 && num > num3) {
+            System.out.println(num);
+        }
+        else if(num2 > num && num2 > num3){
+            System.out.println(num2);
+        }
+        else{
+            System.out.println(num3);
+        }
+  }  
+}

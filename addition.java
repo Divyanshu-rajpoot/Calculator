d4a606b6f3dad45deaccd49f9ebeb31b7506302e
Divyanshu-rajpoot <divy.r.007@gmail.com>
2025-01-17 17:17:56 +0530

    addition

--- addition.java
+++ addition.java
@@ -6,7 +6,7 @@ public class addition {
         Scanner sc = new Scanner(System.in);
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
-
-        System.out.println(num1 + num2);
+        int num3 = sc.nextInt();
+        System.out.println(num1 + num2 + num3);
     }
 }

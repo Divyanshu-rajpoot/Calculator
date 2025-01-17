2e2229f3bbff01a45feac4860d25fd942756fb7b
Divyanshu-rajpoot <divy.r.007@gmail.com>
2025-01-17 17:25:28 +0530

    main

--- multiply.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class multiply {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println(sc.nextInt() * sc.nextInt() * sc.nextInt());
-    }
-}
